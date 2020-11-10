7e6faf41b4867471377f0a7fbd70bcf79b1afd07
Derek <derek84worth@gmail.com>
2020-11-09 20:21:10 -0700

    Initial upload
    Staring github repo for sharing with Alex.

--- src/pushboxsolver/PushBoxSolver.java
+++ src/pushboxsolver/PushBoxSolver.java
@@ -1,15 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pushboxsolver;
 
 import java.util.LinkedList;
 import java.util.Stack;
 
 /**
- *
+ * Started on 7 Nov
  * @author derekworth
  */
 public final class PushBoxSolver {
